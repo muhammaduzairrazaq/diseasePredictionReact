7432bd35436131689ac84cc09d8ac50c2f093f29
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2024-01-22 15:03:09 +0500

    added interaction in chatbot

--- disease-prediction/src/components/InteractionPage/ChatBot.js
+++ disease-prediction/src/components/InteractionPage/ChatBot.js
@@ -21,8 +21,15 @@ export const ChatBot = () => {
   const [textareaHeight, setTextareaHeight] = useState(40);
 
   const handleKeyDown = (e) => {
-    if (e.key === "Enter") {
-      setTextareaHeight((prevHeight) => prevHeight + 15);
+    const textarea = document.getElementsByClassName("text-area")[0];
+    if (textarea.value === "") {
+      setTextareaHeight(40);
+    } else if (e.key === "Backspace") {
+      if (textareaHeight > 40) {
+        setTextareaHeight((prevHeight) => prevHeight - 5);
+      }
+    } else if (e.key === "Enter") {
+      setTextareaHeight((prevHeight) => prevHeight + 20);
     }
   };
 
@@ -46,126 +53,150 @@ export const ChatBot = () => {
     };
   }, []);
 
+  const chatbotResponses = [
+    "Great! what’s your name?",
+    "Hi, Uzair tell me how can I help you in your medical assessment?",
+    "Is there any thing else you are feeling about?",
+    "Okay, according to your symptoms you have a fever take proper medicines and rest you will be fine.",
+  ];
+
+  const [counter, setCounter] = useState(1);
+  const chatbotResponse = () => {
+    const chatContainer = document.getElementsByClassName("chat-container")[0];
+    const div = document.createElement("div");
+    if (counter >= chatbotResponses.length) {
+      setCounter(1);
+    }
+    div.classList.add("bot-message-container");
+    const p = document.createElement("p");
+    p.textContent = chatbotResponses[counter];
+    div.appendChild(p);
+    chatContainer.appendChild(div);
+    setCounter((preCount) => preCount + 1);
+  };
+
+  const handleForward = () => {
+    const chatContainer = document.getElementsByClassName("chat-container")[0];
+    const textarea = document.getElementsByClassName("text-area")[0];
+
+    if (textarea.value != "") {
+      const div = document.createElement("div");
+      div.classList.add("user-message-container");
+      const p = document.createElement("p");
+      p.textContent = textarea.value;
+      div.appendChild(p);
+      chatContainer.appendChild(div);
+      textarea.value = "";
+      setTextareaHeight(40);
+      setTimeout(() => {
+        chatbotResponse();
+      }, 2000);
+    }
+  };
+
   return (
     <Container>
       <Row>
         <Col md={12}>
           <div className="main-chatbot-container">
-       
-          <div className="chatbot-container">
-            <div
-              className={`chatbot-message-box${
-                scrolled ? " scrolled-background" : ""
-              }`}
-            >
-              <img className="message-bulb" src={bulb} alt="bulb" />
-              <p>
-                If you’re experiencing serious symptoms, do not use Adax.
-                Instead, please contact emergency services.
-              </p>
-            </div>
-            <img className="bot-logo" src={logo} alt="logo" />
-            <div className="bot-selection">
-              <Link to="/chatbot">
-                <button className="bot-selection-button chatbot-button">
-                  Use ChatBot
-                </button>
-              </Link>
-              <Link to="/voicebot">
-                <OverlayTrigger
-                  placement="top"
-                  overlay={
-                    <Tooltip id="tooltip-top" className="custom-tooltip">
-                      Click to use <strong>VoiceBot</strong>.
-                    </Tooltip>
-                  }
-                >
-                  <button className="bot-selection-button">Use VoiceBot</button>
-                </OverlayTrigger>
-              </Link>
-            </div>
-            <div className="chat-container">
-              <div className="bot-message-container">
-                <p>Great! what’s your name?</p>
-              </div>
-              <div className="user-message-container">
-                <p>Hi, my name is uzair.</p>
-              </div>
-              <div className="bot-message-container">
+            <div className="chatbot-container">
+              <div
+                className={`chatbot-message-box${
+                  scrolled ? " scrolled-background" : ""
+                }`}
+              >
+                <img className="message-bulb" src={bulb} alt="bulb" />
                 <p>
-                  Hi, Uzair tell me how can I help you in your medical
-                  assessment?
+                  If you’re experiencing serious symptoms, do not use Adax.
+                  Instead, please contact emergency services.
                 </p>
               </div>
-              <div className="user-message-container">
-                <p>I am feeling a little dizzy</p>
+              <img className="bot-logo" src={logo} alt="logo" />
+              <div className="bot-selection">
+                <Link to="/chatbot">
+                  <button className="bot-selection-button chatbot-button">
+                    Use ChatBot
+                  </button>
+                </Link>
+                <Link to="/voicebot">
+                  <OverlayTrigger
+                    placement="top"
+                    overlay={
+                      <Tooltip id="tooltip-top" className="custom-tooltip">
+                        Click to use <strong>VoiceBot</strong>.
+                      </Tooltip>
+                    }
+                  >
+                    <button className="bot-selection-button">
+                      Use VoiceBot
+                    </button>
+                  </OverlayTrigger>
+                </Link>
               </div>
-              <div className="bot-message-container">
-                <p>Is there any thing else you are feeling about?</p>
+              <div className="chat-container">
+                <div className="bot-message-container">
+                  <p>Great! what’s your name?</p>
+                </div>
+                {/* <div className="user-message-container">
+                  <p>Hi, my name is uzair.</p>
+                </div> */}
               </div>
-              <div className="user-message-container">
-                <p>
-                  Yeah I am feeling a little cold, pain in back of my head and
-                  flue also.
-                </p>
+              <div className="chatbot-questions">
+                <div className="questions">
+                  <p>How to use Adax?</p>
+                </div>
+                <div className="questions">How can I give voice commands?</div>
+                <div className="questions">Give me a detail tour of Adax?</div>
               </div>
-              <div className="bot-message-container">
-                <p>
-                  Okay, according to your symptoms you have a fever take proper
-                  medicines and rest you will be fine.
-                </p>
-              </div>
-              <div className="user-message-container">
-                <p>Thanks for your suggestions.</p>
-              </div>
-            </div>
-            <div className="chatbot-questions">
-              <div className="questions">
-                <p>How to use Adax?</p>
-              </div>
-              <div className="questions">How can I give voice commands?</div>
-              <div className="questions">Give me a detail tour of Adax?</div>
-            </div>
-            <div className="input-container">
-              <div className="input-box">
-                <textarea
-                  onKeyDown={handleKeyDown}
-                  placeholder="Enter your response here..."
-                  style={{ height: `${textareaHeight}px` }}
-                ></textarea>
-                <FontAwesomeIcon
-                  icon={faMicrophone}
-                  className="chatbot-dots"
-                  style={{ color: "#215cec" }}
-                />
-              </div>
-              <div className="forward-container">
-                <Link to="/diseasereport">
+              <div className="input-container">
+                <div className="input-box">
+                  <textarea
+                    autoFocus
+                    className="text-area"
+                    onKeyDown={handleKeyDown}
+                    placeholder="Enter your response here..."
+                    style={{ height: `${textareaHeight}px` }}
+                  ></textarea>
+                  <FontAwesomeIcon
+                    icon={faMicrophone}
+                    className="chatbot-dots"
+                    style={{ color: "#215cec" }}
+                  />
+                </div>
+                {/* <div className="forward-container">
+                  <Link to="/diseasereport">
+                    <FontAwesomeIcon
+                      icon={faCaretRight}
+                      className="chatbot-dots"
+                      style={{ color: "#215cec" }}
+                    />
+                  </Link>
+                </div> */}
+                <div className="forward-container" onClick={handleForward}>
                   <FontAwesomeIcon
                     icon={faCaretRight}
                     className="chatbot-dots"
                     style={{ color: "#215cec" }}
                   />
-                </Link>
+                </div>
+              </div>
+              <div className="bottom-container">
+                <OverlayTrigger
+                  placement="top"
+                  overlay={
+                    <Tooltip id="tooltip-top" className="custom-tooltip">
+                      Click to <strong>Restart</strong> assessment.
+                    </Tooltip>
+                  }
+                >
+                  <button className="restart-button">
+                    <FontAwesomeIcon icon={faRotateRight} />
+                  </button>
+                </OverlayTrigger>
               </div>
             </div>
-            <div className="bottom-container">
-              <OverlayTrigger
-                placement="top"
-                overlay={
-                  <Tooltip id="tooltip-top" className="custom-tooltip">
-                    Click to <strong>Restart</strong> assessment.
-                  </Tooltip>
-                }
-              >
-                <button className="restart-button">
-                  <FontAwesomeIcon icon={faRotateRight} />
-                </button>
-              </OverlayTrigger>
-            </div>
-          </div>
-          <div className="progress-tracker">
-            <p className="tracker-heading">ChatBot Meter</p>
+            <div className="progress-tracker">
+              <p className="tracker-heading">ChatBot Meter</p>
               <Progress />
             </div>
           </div>
