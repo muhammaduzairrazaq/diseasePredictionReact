0db86480c414915e7ad0acb5a37ac5e05c0c5bb4
muhammaduzairrazaq <muhammaduzairrazaq@gmail.com>
2024-01-28 09:19:09 +0500

    added module

--- disease-prediction/src/components/InteractionPage/ChatBotMeter.js
+++ disease-prediction/src/components/InteractionPage/ChatBotMeter.js
@@ -1,21 +1,39 @@
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import "../../App.css"
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "../../App.css";
 
 function Progress() {
-    const pa = 80;
-    const es = 40;
-    const pp = 100;
-    const et = 25;
+  const pa = 80;
+  const es = 40;
+  const pp = 100;
+  const et = 25;
   return (
     <div>
       <p className="progressbar-tags">ChatBot Message</p>
-      <ProgressBar striped variant="success" animated now={pa} label={`${pa}%`} />
+      <ProgressBar
+        striped
+        variant="success"
+        animated
+        now={pa}
+        label={`${pa}%`}
+      />
       <p className="progressbar-tags">User Response</p>
       <ProgressBar striped variant="info" animated now={es} label={`${es}%`} />
       <p className="progressbar-tags">Text Message</p>
-      <ProgressBar striped variant="warning" animated now={pp} label={`${pp}%`} />
+      <ProgressBar
+        striped
+        variant="warning"
+        animated
+        now={pp}
+        label={`${pp}%`}
+      />
       <p className="progressbar-tags">Voice Message</p>
-      <ProgressBar striped variant="danger" animated now={et} label={`${et}%`} />
+      <ProgressBar
+        striped
+        variant="danger"
+        animated
+        now={et}
+        label={`${et}%`}
+      />
     </div>
   );
 }
--- disease-prediction/src/components/InteractionPage/VoiceRecord.js
+++ disease-prediction/src/components/InteractionPage/VoiceRecord.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
-
-
+import React from "react";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 export const Recored = () => {
-    const startListening = () => SpeechRecognition.startListening({ continuous: true, language: 'en-US' });
-    const stopListening = () => SpeechRecognition.stopListening();
-    const { transcript, browserSupportsSpeechRecognition } = useSpeechRecognition()
-    if (!browserSupportsSpeechRecognition) {
-        return null
-      }
+  const startListening = () =>
+    SpeechRecognition.startListening({ continuous: true, language: "en-US" });
+  const stopListening = () => SpeechRecognition.stopListening();
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
 
   return (
     <div className="main-recorded-container">
-        <div className="text-container-voice">
-            { transcript }
-        </div>
+      <div className="text-container-voice">{transcript}</div>
       <div className="recorded-button">
         <button>Copy</button>
-        <button onClick={ startListening }>Start Listening</button>
-        <button onClick={ stopListening }>Stop Listening</button>
+        <button onClick={startListening}>Start Listening</button>
+        <button onClick={stopListening}>Stop Listening</button>
       </div>
     </div>
   );
